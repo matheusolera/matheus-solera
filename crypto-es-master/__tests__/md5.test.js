3c7681fa5cabfbe0ebd7e92e9aede7a7551a2346
MatheusHPS <69045402+matheusolera@users.noreply.github.com>
2022-07-11 12:55:21 -0300

    remove cripto

--- crypto-es-master/__tests__/md5.test.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-import C from '../lib/index.js';
-
-describe('md5', () => {
-  it('vector 1', () => {
-    expect(C.MD5('').toString()).toBe('d41d8cd98f00b204e9800998ecf8427e');
-  });
-});
